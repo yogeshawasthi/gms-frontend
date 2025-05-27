60ac5142aec4957e2409fb419ba25b7b00c1e6d2
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2025-05-27 22:13:04 +0545

    membership fetched successfully

--- src/Components/Addmembership/addmemberShip.js
+++ src/Components/Addmembership/addmemberShip.js
@@ -13,12 +13,19 @@ const AddmemberShip = ({ handleClose }) => {
     console.log(inputField)
 
     const fetchMembership = async () => {
-        // .       
-        // .
-        // Please Watch the youtube video for full code 
-        // .
-        // .
-        // .
+        await axios.get("http://localhost:4000/plans/getMembership", {
+            params: inputField,
+            withCredentials: true
+        }).then((res) => {
+            setMembership(res.data.membership); 
+            toast.success(res.data.membership.length + " Memberships Fetched", {
+              toastId: "membership-fetch-success"
+            });
+            console.log(res.data);
+        }).catch((err) => {
+            console.log(err);
+            toast.error("Error fetching membership details");
+        });
     }
 
     useEffect(() => {
@@ -37,19 +44,20 @@ const AddmemberShip = ({ handleClose }) => {
     return (
         <div className='text-black'>
             <div className='flex flex-wrap gap-5 items-center justify-center'>
-                {/* block for membership details */}
-                <div className='text-lg bg-slate-900 text-white border-2 pl-2 pr-2 flex-col gap-3 justify-between  cursor-pointer pt-1 pb-1 rounded-xl font-semibold hover:bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>
-                    <div>1 months Membership</div>
-                    <div>RS 1000</div>
+                {
+                    membership.map((item, index) => {
+                        return (
+                            <div className='text-lg bg-slate-900 text-white border-2 pl-2 pr-2 flex-col gap-3 justify-between  cursor-pointer pt-1 pb-1 rounded-xl font-semibold hover:bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>
+                                <div> {item.months} months Membership</div>
+                                <div>RS {item.price}</div>
 
-                </div>
+                            </div>
+                        )
+                    })
+                }
 
 
-                <div className='text-lg bg-slate-900 text-white border-2 pl-2 pr-2 flex-col gap-3 justify-between cursor-pointer pt-1 pb-1 rounded-xl font-semibold hover:bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>
-                    <div>2 months Membership</div>
-                    <div>RS 1800</div>
 
-                </div>
             </div>
 
             <hr className='mt-10 mb-10' />
