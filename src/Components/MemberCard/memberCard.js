ef6c97f369cd4accc60e6bbdcb516d073836ad66
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2025-05-31 23:37:47 +0545

    .

--- src/Components/MemberCard/memberCard.js
+++ src/Components/MemberCard/memberCard.js
@@ -8,7 +8,7 @@ const MemberCard = ({ item }) => {
 
         <Link to={`/member/${item?._id}`} className='bg-white rounded-lg p-3 hover:bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:text-white cursor-pointer'>
             <div className='w-28 h-28 flex justify-center relative items-center border-2 p-1 mx-auto rounded-full'>
-                <img className='w-full h-full rounded-full' src='https://i.pinimg.com/564x/3c/66/72/3c6672d8def70a54f2b35025f9abb29f.jpg' alt='Profile pic' />
+                <img className='w-full h-full rounded-full' src={item?.profilePic} alt='Profile pic' />
                 <CircleIcon className='absolute top-0 left-0' sx={{ color: "greenyellow" }} />
             </div>
 
