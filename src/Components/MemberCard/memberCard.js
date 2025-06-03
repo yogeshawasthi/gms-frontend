a935380783c4b73fee8537ec8ec61421f5cf6c9d
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2025-06-03 20:54:51 +0545

    idk

--- src/Components/MemberCard/memberCard.js
+++ src/Components/MemberCard/memberCard.js
@@ -9,20 +9,20 @@ const MemberCard = ({ item }) => {
             className='bg-white rounded-lg p-3 cursor-pointer group transition duration-300 hover:shadow-2xl hover:scale-105 hover:bg-gradient-to-br hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500'
         >
             <div className='w-28 h-28 flex justify-center relative items-center border-2 p-1 mx-auto rounded-full'>
-                <img className='w-full h-full rounded-full' src='https://i.pinimg.com/564x/3c/66/72/3c6672d8def70a54f2b35025f9abb29f.jpg' alt='Profile pic' />
-                <CircleIcon className='absolute top-0 left-0' sx={{ color: "greenyellow" }} />
+                <img className='w-full h-full rounded-full' src={item?.profilePic} alt='Profile pic' />
+                <CircleIcon className='absolute top-0 left-0' sx={{ color: item?.status === "Active" ? "greenyellow" : "red" }} />
             </div>
 
-            <div className='mx-auto mt-5 text-center text-xl font-semibold font-mono'>
-                {"_*_"}
+            <div className='mx-auto mt-5 text-center text-xl font-semibold font-mono group-hover:text-white'>
+                {item?.name}
             </div>
 
-            <div className='mx-auto mt-2 text-center text-xl font-mono'>
-                {"+977" + "9xxxxxxxx"}
+            <div className='mx-auto mt-2 text-center text-xl font-mono group-hover:text-white'>
+                {item?.mobileNo}
             </div>
 
-            <div className='mx-auto mt-2 text-center text-xl font-mono'>
-                Next Bill Date : {"2082-12-30"}
+            <div className='mx-auto mt-2 text-center text-xl font-mono group-hover:text-white'>
+                Next Bill Date : {item?.nextBillDate.slice(0, 10).split('-').reverse().join('-')}
             </div>
         </Link>
     )
