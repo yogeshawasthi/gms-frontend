318fe58fe1bdfb321fa97196c08402ec12f14c49
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2025-06-11 22:28:46 +0545

    soft gradiant to member card added more professional looking

--- src/Components/MemberCard/memberCard.js
+++ src/Components/MemberCard/memberCard.js
@@ -14,22 +14,22 @@ const MemberCard = ({ item, header }) => {
     return (
         <Link
             to={`/member/${item?._id}`}
-            className='bg-white rounded-lg p-3 cursor-pointer group transition duration-300 hover:shadow-2xl hover:scale-105 hover:bg-gradient-to-br hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500 h-80 flex flex-col justify-start'
+            className='bg-white rounded-lg p-3 cursor-pointer group transition duration-300 hover:shadow-xl hover:bg-blue-50 h-80 flex flex-col justify-start'
         >
             <div className='w-28 h-28 flex justify-center relative items-center border-2 p-1 mx-auto rounded-full'>
                 <img className='w-full h-full rounded-full' src={item?.profilePic} alt='Profile pic' />
                 <CircleIcon className='absolute top-0 left-0' sx={{ color: dotColor }} />
             </div>
 
-            <div className='mx-auto mt-5 text-center text-xl font-semibold font-mono text-black group-hover:text-white'>
+            <div className='mx-auto mt-5 text-center text-xl font-semibold font-mono text-black group-hover:text-blue-700'>
                 {item?.name}
             </div>
 
-            <div className='mx-auto mt-2 text-center text-xl font-mono text-black group-hover:text-white'>
+            <div className='mx-auto mt-2 text-center text-xl font-mono text-black group-hover:text-blue-700'>
                 {item?.mobileNo}
             </div>
 
-            <div className='mx-auto mt-2 text-center text-xl font-mono text-black group-hover:text-white'>
+            <div className='mx-auto mt-2 text-center text-xl font-mono text-black group-hover:text-blue-700'>
                 Next Bill Date : {item?.nextBillDate?.slice(0, 10).split('-').reverse().join('-')}
             </div>
         </Link>
